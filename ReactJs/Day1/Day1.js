688ebbbe958cdb3292fc7d66a394e209dd529542
Gauri Mahashabde <gauri2interact@gmail.com>
2023-09-24 20:08:45 +0530

    Questions for Day 1

---
+++ ReactJs/Day1/Day1.js
@@ -0,0 +1,4 @@
+/*
+What is react ?
+=>React is  a open source javascript library used to develope complex and interactive web and mobile UI.
+=>Developer primarily use react to create single page applications and the library focuses solely on the view layer of MVC.*/
