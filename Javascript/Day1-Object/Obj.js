7821ef3b48ee4d2439a125b935a6fe32ac794c55
Gauri Mahashabde <gauri2interact@gmail.com>
2023-09-28 19:32:07 +0530

    JS questions on Object

--- Javascript/Day1-Object/Obj.js
+++ Javascript/Day1-Object/Obj.js
@@ -45,3 +45,37 @@ for (let property in students){
     console.log(`${property}:${students[property]}`)
 }
 
+//constructor function
+//A constructor function is a function that is used to create new objects with the same properties and methods.
+
+//Creation
+
+function Person (name , age){
+    this.name = name;
+    this.age = age;
+}
+
+let person1 =  new Person("Gauri" , 24);
+let person2 =new Person("chaitre" , 20);
+let person3 =  new Person("joy" , 45);
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
+
+//prototypal inheritance 
+//Prototypal inheritance is a way of creating new objects based on an existing object.
+// The new object inherits the properties and methods of the existing object.
+//creation
+let him = {
+    nam :"john",
+    age:30,
+    greet:function(){
+            console.log("Hello ")
+    }   
+}
+
+let employeer = Object.create(him);
+employeer.jobtitle = 'software Developer'
+
+console.log(him);
