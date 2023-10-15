872d64fbcc2b97fdc5a3737fd4ca8ed4ed57e615
Gauri <gauriM304@gmail.com>
2023-10-15 18:56:41 +0530

    async and await

--- Javascript/Day2-PCA/index.js
+++ Javascript/Day2-PCA/index.js
@@ -88,3 +88,26 @@ getSumNum(1, 3).then(data => {
   console.log(err)
 })
 
+//Async and await
+
+
+function asynchronous_operational_method() {
+    let first_promise = 
+        new Promise((resolve, reject) => resolve("Hello"));
+    let second_promise = 
+        new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(" GeeksforGeeks..");
+        }, 1000);
+    });
+    let combined_promise = 
+        Promise.all([first_promise, second_promise]);
+    return combined_promise;
+}
+ 
+async function display() {
+    let data = await asynchronous_operational_method();
+    console.log(data);
+}
+ 
+display();
