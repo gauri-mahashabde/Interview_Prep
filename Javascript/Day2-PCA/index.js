a83ecc132ab0ece06a51cd7aebe9dea00d8ab426
Gauri <gauriM304@gmail.com>
2023-10-15 17:02:48 +0530

    Callback

---
+++ Javascript/Day2-PCA/index.js
@@ -0,0 +1,14 @@
+//callback
+/*A callback function is a function in JavaScript that is called (or invoked) by the first function after the first function has completed its task. It’s a function that receives input from another function. 
+They are often used to handle asynchronous events and make your code more readable. 
+Importance
+They allow you to handle events
+You can use them to send asynchronous API calls
+You can use them to enhance your code’s performance
+Callback functions can be used to manage the flow of asynchronous operations, preventing the infamous “callback hell.” 
+Closures, which allow functions to “remember” the scope in which they were generated, are frequently used by callback functions.
+Higher-order functions, or functions that can take other functions as inputs or return other functions as values, are based on the concept of callbacks. 
+Use
+Update the UI after a network request has completed.
+Process data after a file has been read.
+Make another API call after the results of the first API call have been received.*/
