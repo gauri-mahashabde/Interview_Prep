fa0d7f9d5efa74ec00f1e087f81be60aa7e84342
Gauri Mahashabde <96432142+gauri-mahashabde@users.noreply.github.com>
2023-10-17 20:27:51 +0530

    Update Day1.js

--- ReactJs/Day1/Day1.js
+++ ReactJs/Day1/Day1.js
@@ -2,3 +2,13 @@
 What is react ?
 =>React is  a open source javascript library used to develope complex and interactive web and mobile UI.
 =>Developer primarily use react to create single page applications and the library focuses solely on the view layer of MVC.*/
+
+What are the main features of react?
+JSX (JavaScript Syntax Extension)
+Virtual DOM
+One-way data binding
+Performance
+Extensions
+Conditional statements
+Components
+Simplicity  
