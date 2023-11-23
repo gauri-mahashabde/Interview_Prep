8092a524d170b7708067a8804a4ac4f300721542
Gauri <gauriM304@gmail.com>
2023-11-23 20:26:43 +0530

    Default props

--- ReactJs/consultation_app/src/index.js
+++ ReactJs/consultation_app/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-//import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 //import Comp from './Component/Comp'
-import Compclass from './Component/Compclass'
-import Parentcomponent from './Component/Parentcomponent';
+import Compclass from "./Component/Compclass";
+//import Parentcomponent from './Component/Parentcomponent';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.Fragment>
-    <Parentcomponent/>
+    <App />
   </React.Fragment>
 );
 
