896c4355f35d17c9b9b499a4b0a2ce4f6d9b5f0a
Gauri <gauriM304@gmail.com>
2023-11-23 21:25:48 +0530

    default props

--- ReactJs/consultation_app/src/Component/ConsultationCard.js
+++ ReactJs/consultation_app/src/Component/ConsultationCard.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-function ConsultationCard() {
+function ConsultationCard({ userName, consultationDate, topic }) {
   return (
-    <div>ConsultationCard</div>
-  )
-}
+    <div className="consultation-card">
+            <h3>{userName}</h3>
+            <p>Date: {consultationDate}</p>
+            <p>Topic: {topic}</p>
+        </div>
+ 
+ )
+ 
+};
+//default props
+ConsultationCard.defaultProps = {
+  userName: 'Anonymous',
+  consultationDate: 'N/A',
+  topic: 'General'
+};
+
+
+
+
 
 export default ConsultationCard
--- ReactJs/consultation_app/src/Component/ContentCard.js
+++ ReactJs/consultation_app/src/Component/ContentCard.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-function ContentCard() {
+function ContentCard({title , description , author}) {
   return (
-    <div>ContentCard</div>
+    <div className='content-card'>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <p>author:{author}</p>
+    </div>
   )
-}
+};
+
+// Default props
+ContentCard.defaultProps = {
+  title: 'Untitled',
+  description: 'No description available',
+  author: 'Unknown'
+};
+
 
 export default ContentCard
